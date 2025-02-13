ba7d9f0da248028741b65697d2b75fe39e6d9823
Sareena <sareenamann@berkeley.edu>
2025-02-12 23:09:29 -0800

    done

---
+++ homework2/homework2/hw2.py
@@ -0,0 +1,32 @@
+<<<<<<< HEAD
+# 1) Suppose your current working directory is ~/Desktop/classes/python_decal/. What command would allow you to navigate directly to ~/Desktop?
+#       cd ~/Desktop
+||||||| parent of 1d0aa07 (finished hw2)
+# 1) Suppose your current working directory is ~/Desktop/classes/python_decal/. What command would allow you to navigate directly to ~/Desktop?
+=======
+1) Suppose your current working directory is ~/Desktop/classes/python_decal/. What command would allow you to navigate directly to ~/Desktop?
+#       cd ~/Desktop
+>>>>>>> 1d0aa07 (finished hw2)
+# 2) What does ~/ mean?
+#       The home directory
+# 3) What's the difference between and abolute and a relative path in your terminal?
+#       An absolute path is exactly where, from start to end, a directory is located while a relative path is how to get to another directory from the directory you are currently on.
+# 4) What command would bring you back to your home directory?
+#       cd
+# 5) If you called rm ./ in your current working directory, what would it do? 
+#       rm ./* would delete all files in current directory (not rm ./).
+# 6) In your current working directory, what does calling "git add" do? What about "git commit"? What about "git push"?
+#       Git add stages a change, git commit saves the changes to a local repo, and git push sends the changes to the remote repo.
+# 7) Let's say you call "git add" in your current working directory and then "git status". What message would appear? What is it telling you?
+#       It would say files are ready to commit, but they haven't been commited yet. It is telling you to commit.
+# 8) What has been the most frustrating error or bug you've encountered in this class so far? How did you troubleshoot and resolve it?
+#       As of now, none!
+# 9) How does cloning a repository differ from pulling from a repository?
+#       Cloning creates a local copy of the fill while pulling updates the existing file.
+# 10) Tell me a fun fact!
+<<<<<<< HEAD
+#       My dog and I share the same bday!
+||||||| parent of 1d0aa07 (finished hw2)
+=======
+#       My dog and I share the same bday (August 23)!
+>>>>>>> 1d0aa07 (finished hw2)
