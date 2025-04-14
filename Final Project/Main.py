cf8096744bed073bca1889d86a8b492310413231
Sareena <sareenamann@berkeley.edu>
2025-04-13 18:10:25 -0700

    working on gui

--- Final Project/Main.py
+++ Final Project/Main.py
@@ -1,3 +1,28 @@
-print("Welcome to My Near Earth Comets Animation. Please enter a desired near-Earth Comet from the following list:")
-print("")
-cometName = input("Enter desired near-Earth Comet")
+import tkinter as tk
+
+# Creates new Window
+root = tk.Tk()
+root.geometry("900x1200")
+root.title("Near Earth Comet Simulator")
+
+label = tk.Label(root, text="Welcome to the Near Earth Comet Simulator!", font = ('Arial', 20))
+label.pack(padx=10, pady=10)
+
+# Basic information about why I am doing this, aim, background on near Earth objects
+
+
+#Something the user can type into
+txtbox = tk.Entry(root, width = 20)
+txtbox.pack(padx=10)
+
+button = tk.Button(root, text= "Animate!", font = ('Arial', 20))
+# Make stickY??
+button.pack(padx=10, pady=10)
+
+
+#List of all options to choose from
+
+#Maybe another window that has the table
+
+root.mainloop()
+
